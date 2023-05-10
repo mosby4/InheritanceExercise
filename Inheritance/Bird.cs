fce7d8f4d3c80773dcbc59f59d3fba2ce0487928
Mosby4 <rufus.hamby@gmial.com>
2023-05-10 11:19:53 -0500

    Classes created

---
+++ Inheritance/Bird.cs
@@ -0,0 +1,11 @@
+﻿using System;
+namespace Inheritance
+{
+	public class Bird
+	{
+		public Bird()
+		{
+		}
+	}
+}
+
---
+++ Inheritance/Reptile.cs
@@ -0,0 +1,11 @@
+﻿using System;
+namespace Inheritance
+{
+	public class Reptile
+	{
+		public Reptile()
+		{
+		}
+	}
+}
+
